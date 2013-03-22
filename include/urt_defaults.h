1e169205e9df8306d19525b45c93085bfc13ec8f
Shahbaz Youssefi <ShabbyX@gmail.com>
2013-03-22 07:18:44 +0100

    cleared up meaning of unused parameter

--- include/urt_defaults.h
+++ include/urt_defaults.h
@@ -48,8 +48,8 @@
 #define urt_shsem_attach(...) urt_shsem_attach(__VA_ARGS__, NULL)
 #define urt_sem_wait(...) urt_sem_wait(__VA_ARGS__, NULL)
 /* a helper macro is used for `, ##__VA_ARGS__` to correctly expand */
-#define urt_rwlock_new(...) urt_rwlock_new_(1, ##__VA_ARGS__, NULL)
-#define urt_rwlock_new_(n, ...) urt_rwlock_new(__VA_ARGS__)
+#define urt_rwlock_new(...) urt_rwlock_new_(unused, ##__VA_ARGS__, NULL)
+#define urt_rwlock_new_(unused, ...) urt_rwlock_new(__VA_ARGS__)
 #define urt_shrwlock_new(...) urt_shrwlock_new(__VA_ARGS__, NULL)
 #define urt_shrwlock_attach(...) urt_shrwlock_attach(__VA_ARGS__, NULL)
 #define urt_rwlock_read_lock(...) urt_rwlock_read_lock(__VA_ARGS__, NULL)
