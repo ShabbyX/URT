4968a658652be51fceb6c0f5e6f6ea8d2330635a
Shahbaz Youssefi <ShabbyX@gmail.com>
2013-07-26 18:59:57 +0200

    urt_sleep now callable from non-real-time context
    Signed-off-by: Shahbaz Youssefi <ShabbyX@gmail.com>

--- sys/rtai/include/urt_sys_time.h
+++ sys/rtai/include/urt_sys_time.h
@@ -22,15 +22,60 @@
 
 #ifdef __KERNEL__
 # include <linux/kernel.h>
+# include <asm/div64.h>
+#else
+# include <unistd.h>
 #endif
 #include <rtai_lxrt.h>
+#include "urt_sys_utils.h"
 
 URT_DECL_BEGIN
 
 typedef int64_t urt_time;
 
 #define urt_get_time rt_get_time_ns
-#define urt_sleep(t) rt_sleep(nano2count(t))
+static inline void urt_sleep(urt_time t)
+{
+	if (urt_is_rt_context())
+		rt_sleep(nano2count(t));
+	else
+	{
+#ifdef __KERNEL__
+		if (t <= 0)
+			return;
+
+		t += 999;
+		do_div(t, 1000);	/* round up to micro seconds */
+
+		/* per kernel doc recommendations: Documentation/timers/timers-howto.txt */
+		if (t < 10)
+			udelay(t);
+		else if (t < 15000)
+			/* a ~1.5% slack given, just for the heck of it */
+			usleep_range(t, t + t / 64);
+		else
+		{
+			/* the time is above 10s of ms.  The us part of it can be ignored and the time rounded up */
+			t += 999;
+			do_div(t, 1000);
+			msleep(t);
+		}
+#else
+		/* Note: this part is the same as POSIX's urt_sleep */
+		urt_time ts, tu;
+		if (t <= 0)
+			return;
+
+		/* Note: on some systems, usleep cannot be called for values greater than 1,000,000 */
+		t = (t + 999) / 1000;	/* round up to micro seconds */
+		ts = t / 1000000;
+		tu = t % 1000000;
+
+		sleep(ts);
+		usleep(tu);
+#endif
+	}
+}
 
 static inline urt_time urt_get_exec_time(void)
 {
