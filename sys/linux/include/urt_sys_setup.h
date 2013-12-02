085b844be8824a9151fa2ca6be26e7f52e5750ba
Shahbaz Youssefi <ShabbyX@gmail.com>
2013-12-02 17:07:06 +0100

    [update] move glue code out of sys
    There would be a lot of duplicate code otherwise since all user-space
    applications would probably have the same glue code and all kernel ones
    have the same glue code too.
    
    Note: this may not end up being true in case of non-POSIX systems, ones
    that are not completely compliant or kernels other than Linux.
    
    Signed-off-by: Shahbaz Youssefi <ShabbyX@gmail.com>

--- sys/linux/include/urt_sys_setup.h
+++ sys/linux/include/urt_sys_setup.h
@@ -28,52 +28,6 @@ URT_DECL_BEGIN
 static inline int urt_sys_init(void) { return URT_SUCCESS; }
 static inline void urt_sys_exit(void) {}
 
-#define URT_MODULE_LICENSE(...)
-#define URT_MODULE_AUTHOR(...)
-
-#define URT_GLUE(init, body, exit, interrupted, done)	\
-static volatile sig_atomic_t interrupted = 0;		\
-static int done = 0;					\
-static void urt_sig_handler_(int signum)		\
-{							\
-	interrupted = 1;				\
-}							\
-int main(int argc, char **argv)				\
-{							\
-	int err;					\
-	/* set signal handler */			\
-	struct sigaction sa;				\
-	sa = (struct sigaction){.sa_handler = NULL};	\
-	sa.sa_handler = urt_sig_handler_;		\
-	sigemptyset(&sa.sa_mask);			\
-	sigaction(SIGINT, &sa, NULL);			\
-	sigaction(SIGHUP, &sa, NULL);			\
-	sigaction(SIGTERM, &sa, NULL);			\
-	sigaction(SIGQUIT, &sa, NULL);			\
-	sigaction(SIGUSR1, &sa, NULL);			\
-	sigaction(SIGUSR2, &sa, NULL);			\
-	err = init();					\
-	if (err)					\
-		return err;				\
-	body();						\
-	/* wait for done */				\
-	while (!done)					\
-		usleep(10000);				\
-	exit();						\
-	return 0;					\
-}
-
-#define URT_GLUE_NO_INTERRUPT(init, body, exit)		\
-int main(int argc, char **argv)				\
-{							\
-	int err = init();				\
-	if (err)					\
-		return err;				\
-	body();						\
-	exit();						\
-	return 0;					\
-}
-
 URT_DECL_END
 
 #endif
